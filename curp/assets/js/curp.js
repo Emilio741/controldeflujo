dfce4aacb385d65104572eb825da4c9ca68e0b31
Emilio <78672531+Emilio741@users.noreply.github.com>
2022-05-05 15:53:22 -0500

    Add files via upload

---
+++ curp/assets/js/curp.js
@@ -0,0 +1,28 @@
+// Ingresar datos
+alert("Hola buen día")
+var app = prompt("Ingrese su apellido paterno: ");
+app = app.toUpperCase();
+
+var apm = prompt("Ingrese su apellido materno: ");
+apm = apm.toUpperCase()
+
+var nombre= prompt("Ingrese su PRIMER nombre")
+nombre = nombre.toUpperCase()
+
+var anio= prompt("INGRESE SU AÑO DE NACIMIENTO")
+
+var mes= prompt("Ingrese su  mes en numero")
+
+var dia= prompt("ingrese su dia de nacimiento")
+
+var sexo= prompt("Ingrese su sexo, si eres hombre pon H u Hombre ,si eres mujeres pon M o Mujer")
+// convierte datos
+sexo = sexo.toUpperCase()
+
+var entidad= prompt("Ingrese el estado donde residue: ")
+
+
+alert("Hola " + app[0]+app[1]+ apm[0] +nombre[0]+ anio[2] + anio[3] + mes[0]+mes[1]+dia[0]+dia[1]+ sexo[0] + "NLMRCA7")
+                // MR SON LAS CONSONANTES QUE SON REPITEN LEMUS M MORALES R MR
+// conversion cadena a entero
+//switch
